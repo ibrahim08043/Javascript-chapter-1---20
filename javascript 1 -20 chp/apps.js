0bf4264686e03d77b4cb536deeeda338c6a2636a
Ibrahim Kashif <111134217+ibrahim08043@users.noreply.github.com>
2022-09-03 00:38:59 -0700

    Add files via upload

---
+++ javascript 1 -20 chp/apps.js
@@ -0,0 +1,68 @@
+//chapter no 1
+
+// alert("Error! Please enter a valid password");
+// alert("Welcome to JS Land...Happy Coding!");
+// alert("Welcome to JS Land...");
+// alert("Hello... i can run JS through my web browser's console");
+
+//chapter no 2
+
+//var username = "JhonDoe"
+//alert(username);
+//var myname = "15 year old"
+//alert(myname);
+//var message = "Certified Mobile Application Development"
+//alert(message);
+//var pizza = "PIZZA PIZZ PIZ PI P"
+//alert(pizza);
+//var email = "example@example.com"
+//alert(email);
+//var book = "I am trying to learn from the book A smarter way to learn Javascript"
+//alert(book);
+//var msg = "Yah! I can write HTML content through JavaScript";
+//document.write(msg);
+//var end = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+//alert(end);
+
+//chaptet no 3
+
+//var myname = "I am 15 years old"
+//alert(myname);
+//var visit = "You have visited this site 14 times"
+//alert(visit);
+//var birthyear = "My birth year is 1990 Data type of my declared varible is number"
+//document.write(birthyear);
+//var clothingstore ="JohnDoe ordered 5 T-shirt(s) on XYZ Clothing store "
+//document.write(clothingstore);
+
+//chapter no 4
+
+//var heading = "Rules for naming JS variables"
+//document.write(heading);
+//var legal = "Variable names can only contain number,$ and _. For example $my_1stVariable"
+//document.write(legal);
+//var illegal = "Variables must begin with a letter,$ or _. For example $name, _name or name"
+//document.write(illegal);
+//var sensitive = "Variable names are case sensitive"
+//document.write(sensitive);
+//var keyword = "Variable names should not be JS keywords"
+//document.write(keyword);
+
+//chaptet no 5
+
+//var a = 5
+//var b = 3
+//var c = a+b
+//document.write(c);
+//var a = 5
+//var b = 3
+//var c = a-b
+//document.write(c);
+//var a = 5
+//var b = 3
+//var c = a*b
+//document.write(c);
+//var a = 5
+//var b = 3
+//var c = a/b
+//document.write(c);
